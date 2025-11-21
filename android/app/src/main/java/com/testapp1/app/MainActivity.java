ee6cb82946905975de891c324d51d1dd520df1b6
magesh <mageshvaran16@gmail.com>
2025-11-21 11:43:15 +0530

    a

---
+++ android/app/src/main/java/com/testapp1/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.testapp1.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.testapp1.app',
+  appName: 'TESTAPP!',
+  webDir: 'dist'
+};
+
+export default config;
